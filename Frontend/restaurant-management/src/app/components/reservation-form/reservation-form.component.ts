ebab8b28ced752896a3eb5c6e10381962f97e2c5
Khushbu Thakkar <Khushbu.thakkar@techextensor.com>
2024-12-25 01:16:42 +0530

    clean up

--- Frontend/restaurant-management/src/app/components/reservation-form/reservation-form.component.ts
+++ Frontend/restaurant-management/src/app/components/reservation-form/reservation-form.component.ts
@@ -50,9 +50,9 @@ export class ReservationFormComponent implements OnInit {
   }
 
   submitForm(form:NgForm): void {
-    if(form.invalid){
-      return;
-    }
+    // if(form.invalid){
+    //   return;
+    // }
     if (this.guestCount > this.maxGuestCount) {
       this.errorMessage =  'Guest count exceeds the maximum seating capacity of '+this.maxGuestCount;
       return;
